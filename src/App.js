0bea1c3a55c2cc7e00c6c4b80ed5d48beb9e3a1c
teacarolina <teacarolina@icloud.com>
2021-04-26 10:18:32 +0200

    fixed warning

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import AppRoute from "./components/AppRoute";
-import API from "./components/API";
 
 function App() {
   return (
     <div className="App">
      
     <AppRoute/>
-    <API/>
 
     </div>
   );

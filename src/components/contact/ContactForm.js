b43631b97b5da40e3e598a98c6b2e6f46a1edfb1
teacarolina <teacarolina@icloud.com>
2021-07-22 12:32:35 +0200

    env

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -3,12 +3,14 @@ import Header from "../header/Header";
 import Footer from "../footer/Footer";
 import emailjs from 'emailjs-com';
 
+require('dotenv').config()
+
 function ContactForm() {
 
     function sendEmail(e) {
         e.preventDefault();
-                            //service id          //template id              user id 
-        emailjs.sendForm('service_2ozejt9', 'template_bop20e8', e.target, 'user_jlta0kUXn9CQS5Vi3JDFs')
+                            //service id                              //template id                       //user id 
+        emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, e.target, process.env.REACT_APP_USER_ID)
           .then((result) => {
               console.log(result.text);
           }, (error) => {
@@ -33,9 +35,9 @@ function ContactForm() {
                         </p>
 
                         <div className="flex items-center mt-8 text-gray-600 dark:text-gray-400">
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" className="w-8 h-8 text-gray-500">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/>
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"/>
+                            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" className="w-8 h-8 text-gray-500">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/>
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"/>
                             </svg>
                             <div className="ml-4 text-md tracking-wide font-semibold w-40">
                                 Stylist Studion
@@ -43,8 +45,8 @@ function ContactForm() {
                         </div>
 
                         <div className="flex items-center mt-4 text-gray-600 dark:text-gray-400">
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" className="w-8 h-8 text-gray-500">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"/>
+                            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" className="w-8 h-8 text-gray-500">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"/>
                             </svg>
                             <div className="ml-4 text-md tracking-wide font-semibold w-40">
                                 +44 1234567890
@@ -52,8 +54,8 @@ function ContactForm() {
                         </div>
 
                         <div className="flex items-center mt-2 text-gray-600 dark:text-gray-400">
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" className="w-8 h-8 text-gray-500">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
+                            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" className="w-8 h-8 text-gray-500">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
                             </svg>
                             <div className="ml-4 text-md tracking-wide font-semibold w-40">
                                 info@styliststudion.se
@@ -63,17 +65,17 @@ function ContactForm() {
 
                     <form onSubmit={sendEmail} className="p-6 flex flex-col justify-center">
                         <div className="flex flex-col">
-                            <label for="name" className="hidden">Full Name</label>
+                            <label htmlFor="name" className="hidden">Full Name</label>
                             <input type="name" name="name" id="name" placeholder="Full Name" className="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"/>
                         </div>
 
                         <div className="flex flex-col mt-2">
-                            <label for="email" className="hidden">Email</label>
+                            <label htmlFor="email" className="hidden">Email</label>
                             <input type="email" name="email" id="email" placeholder="Email" className="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"/>
                         </div>
 
                         <div className="flex flex-col mt-2">
-                            <label for="message" className="hidden">Subject</label>
+                            <label htmlFor="message" className="hidden">Subject</label>
                             <input type="text" name="message" id="message" placeholder="Message" className="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"/>
                         </div>
 
